3a98a0ba72a39c97348692731bc919b97a2b241d
Tamerlan Klishev <tamerlan.klishev@axxonsoft.dev>
2026-05-25 22:42:19 +0300

    техкомит

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
 }
 
 kotlin {
@@ -38,7 +37,7 @@ kotlin {
             implementation(libs.koin.compose)
             //implementation(libs.koin.compose.viewmodel)
             //implementation(libs.lifecycle.viewmodel)
-            implementation(libs.navigation.compose)
+            implementation(libs.navigation3.ui)
 
         }
 
